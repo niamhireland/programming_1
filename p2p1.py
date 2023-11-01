5687a201228021ec074f8076856295d1ba061fae
Niamh <123694515+niamhireland@users.noreply.github.com>
2023-11-01 17:13:31 +0000

    Create p2p1.py

---
+++ p2p1.py
@@ -0,0 +1,8 @@
+#TASK
+#Write a program that uses a single print command with a number of arguments to print to the screen the strings “Hello,” and “world.” 
+#The output should include a space between the comma and the word “world”.
+
+greeting = "Hello,"
+planet = "world."
+
+print(greeting, planet)
