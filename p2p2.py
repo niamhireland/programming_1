6a97e20d90431cd71d002f25a95f64fb8e8be24c
Niamh <123694515+niamhireland@users.noreply.github.com>
2023-11-01 17:31:53 +0000

    Create p2p2.py

---
+++ p2p2.py
@@ -0,0 +1,8 @@
+#TASK 
+#Write a program that uses a single print command with a single argument to print to the screen the concatenation of the strings “Hello,” and “world.” 
+#Again, the output should include a space between the comma and the word “world”.
+
+greeting = "Hello, "
+planet = "world."
+
+print(greeting + planet)
