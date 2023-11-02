c3e58e52a4a88cacb309535cf5130c1b4448e77e
Niamh <123694515+niamhireland@users.noreply.github.com>
2023-11-02 10:51:37 +0000

    Create p2p3.py

---
+++ p2p3.py
@@ -0,0 +1,6 @@
+# TASK
+#Write a program that assigns to a variable the concatenation of the strings “Hello,” and “world.” and includes a space between the comma and the word “world”. 
+#The program should then print out the value of this variable.
+
+concatenated_string = "Hello, " + "world."
+print(concatenated_string)
