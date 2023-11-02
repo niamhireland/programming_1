043eaa223e1925e923bcd7fefa93094bf7162d81
Niamh <123694515+niamhireland@users.noreply.github.com>
2023-11-02 10:54:06 +0000

    Create p2p4.py

---
+++ p2p4.py
@@ -0,0 +1,22 @@
+#TASK
+#A string in Python is a sequence of characters. You can access the characters one at a time using the index, an expression in square brackets. 
+#The index indicates which letter is required from the string. For example, consider the following code segment:
+# a n im a l = ' e l e p h a n t '
+# l e t t e r = a n im a l [ 1 ]
+# Use different values for the index and use the print command to print out the individual letters selected. Ensure that you understand the behaviour of the indexing. 
+#Submit examples of your experiementation.
+
+#Using the index to print an acrostic word
+animal = "elephant"
+letter = animal[4]
+print (letter)
+
+#Using the index a second time 
+flower = "daisy"
+selection = flower[1]
+print (selection)
+
+#A third attempt at using the index 
+subject = "CompSci"
+vowel = subject[2]
+print (vowel)
